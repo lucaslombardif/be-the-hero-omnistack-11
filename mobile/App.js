a3b0b20071ae34858fc420bea15d4e35c4986cb2
lucaslombardif <lucaas-floriano@hotmail.com>
2020-04-06 21:38:56 -0300

    Atualizando StatusBar

--- mobile/App.js
+++ mobile/App.js
@@ -4,6 +4,9 @@ import 'intl/locale-data/jsonp/pt-BR'
 import React from 'react'
 
 import Routes from './src/routes'
+import { StatusBar } from 'react-native';
+
+StatusBar.setTranslucent(true);
 
 export default function App() {
   return (
