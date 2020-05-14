b47d0c59ad657a539096f33e3ce0e30b69bf5c19
Lucas Lombardi Floriano <lucaas-floriano@hotmail.com>
2020-05-14 13:41:00 -0300

    Permitindo a exposição de header

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,11 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+const options = { 
+    exposedHeaders: 'X-Total-Count'
+}
+
+app.use(cors(options));
 app.use(express.json());
 app.use(routes);
 
